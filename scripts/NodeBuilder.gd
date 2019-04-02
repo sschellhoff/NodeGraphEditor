66a34e2fe98573bfc67476ee60840722cce7b69b
sschellhoff <=soeren.schellhoff@gmail.com>
2019-04-02 09:33:41 +0200

    simplified NodeBuilder and NodeBuildHelper

--- scripts/NodeBuilder.gd
+++ scripts/NodeBuilder.gd
@@ -13,9 +13,15 @@ func get_type_names():
 func get_types():
 	return $Helper.node_types
 
-func register_node_type(name, inputs, outputs):
-	$Helper.register_node_type(name, inputs, outputs)
+func register_node_type(type, inputs, outputs):
+	Logger.info(type + "\t" + str(inputs) + "\t" + str(outputs))
+	$Helper.node_types[type] = {"inputs": inputs, "outputs": outputs}
 
 func create_node(type, position):
+	var node = $Helper.create_empty_node(type, position)
 	var type_data = $Helper.node_types[type]
-	return $Helper.create_node(type, type_data.inputs, type_data.outputs, position)
+	for i in type_data.inputs:
+		$Helper.add_input_slot(node, i, $Helper.SLOT_TYPE.FLOAT)
+	for o in type_data.outputs:
+		$Helper.add_output_slot(node, o, $Helper.SLOT_TYPE.FLOAT)
+	return node
--- scripts/NodeBuilderHelper.gd
+++ scripts/NodeBuilderHelper.gd
@@ -22,23 +22,11 @@ const COLOR_FLOAT = Color(0.5, 0.5, 0.1)
 #                               HELPER METHODS                                 #
 ################################################################################
 
-func register_node_type(type, inputs, outputs):
-	Logger.info(type + "\t" + str(inputs) + "\t" + str(outputs))
-	node_types[type] = {"inputs": inputs, "outputs": outputs}
-
 func create_empty_node(title, position=Vector2()):
 	var node = UsedGraphNode.instance()
 	node.offset = position
 	node.set_title(title)
 	node.type = title
-	return node
-
-func create_node(title, inputs, outputs, position=Vector2()):
-	var node = create_empty_node(title, position)
-	for i in inputs:
-		add_input_slot(node, i, SLOT_TYPE.FLOAT)
-	for o in outputs:
-		add_output_slot(node, o, SLOT_TYPE.FLOAT)
 	return node
 
 func create_label(text):
